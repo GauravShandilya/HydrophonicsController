5b64172e9fee060ff3cbf12e50e28855e397f2ae
gauravshandilya <gauravshandilya.dumca@gmail.com>
2017-10-12 22:10:29 +0530

    Changes2

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ var express=require('express');
 var mongoose=require('mongoose');
 //var db=require('./models/db.js');
 
-//var routes=require('./routes/route.js');
-//var user=require('./routes/user.js');
-//var story=require('./routes/story.js');
+var routes=require('./routes/route.js');
+var configure=require('./routes/configure.js');
+var analyze=require('./routes/analyze.js');
+var script=require('./routes/script.js');
 var bodyParser=require('body-parser');
 
 var session=require('express-session');
@@ -24,9 +25,11 @@ app.use(session({secret:"qazwsxedcrfvtgbyhnujm",resave: true, saveUninitialized:
 
 var port = process.env.PORT || 8080;
 
-app.get('/', function (req, res) {
-  res.send('Hydrophonic Controller Started!')
-})
+app.get('/',routes.index);
+
+// app.get('/', function (req, res) {
+//   res.send('Hydrophonic Controller Started!')
+// })
 var server=app.listen(port,function(req,res){
 
     console.log(chalk.green("Catch the action at http://localhost:"+port));
